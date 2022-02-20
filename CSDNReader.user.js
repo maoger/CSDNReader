6c6e20ea8f0b991506852e7f72f8072d4e6d282c
maoyantsing <maoyantsing@gmail.com>
2022-02-20 19:05:23 +0800

    Update V0.1.1
    根据网站最新情况调整部分代码

--- CSDNReader.user.js
+++ CSDNReader.user.js
@@ -1,37 +1,43 @@
 // ==UserScript==
 // @name         CSDNReader
-// @icon         https://g.csdnimg.cn/static/logo/favicon32.ico
+// @icon         https://blog.csdn.net/favicon.ico
 // @homepage     https://github.com/maoger/CSDNReader
-// @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  CSDN纯净阅读模式
 // @author       Maoger
 // @match        http*://blog.csdn.net/*
-// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
 // @updateURL    https://openuserjs.org/meta/maoger/CSDNReader.meta.js
-// @copyright    2021-2021, maoger (https://openuserjs.org/users/maoger)
+// @downloadURL  https://openuserjs.org/install/maoger/CSDNReader.user.js
+// @copyright    2021-2022, maoger (https://openuserjs.org/users/maoger)
 // @license      MIT
 // ==/UserScript==
 
-(function () {
-  'use strict';
-
-  // 1. 删除
-  $(".pulllog-box").remove(); // 登录框
-  $('#csdn-toolbar').remove(); // 顶部：导航栏
-  $('.blog_container_aside').remove(); // 左侧：导航栏
-  $('.recommend-right').remove();
-  $('.csdn-side-toolbar ').remove();  // 浮动按钮：回到顶部
-  $('.more-toolbox').remove();  // 底部：一键三连
-  $('.comment-box').remove();  // 底部：评论
-  $('.recommend-box').remove(); // 底部：推荐1
-  $('.recommend-tit-mod').remove(); // 底部：推荐2
-  $('.csdn-shop-window-common').remove(); // 底部：空白行
-  $('.template-box').remove(); // 脚注1
-  $('.blog-footer-bottom').remove(); // 脚注2
-
-  // 2. 调整宽度
-  $('.container main').width("100%");
-
-})();
-  
+var locationHref = window.location.href;
+if (locationHref.indexOf('blog.csdn.net') >= 0){
+    setTimeout(function(){
+        CSDN_removeDom_Main();
+    }, 500)
+}
+function CSDN_removeDom_Main() {
+    CSDN_removeDom_Single('.pulllog-box');
+    CSDN_removeDom_Single('#csdn-toolbar');
+    CSDN_removeDom_Single('#blogColumnPayAdvert');
+    let myDoms = document.querySelectorAll('aside.blog_container_aside > div.aside-box');
+    for (let i = 0; i < myDoms.length; i++) {
+        let myDom = myDoms[i];
+        if (myDom.getAttribute('id') != 'asidedirectory') {
+            myDom.parentNode.removeChild(myDom);
+        }
+    }
+    CSDN_removeDom_Single('#rightAside');
+    CSDN_removeDom_Single('.csdn-side-toolbar ');
+    CSDN_removeDom_Single('.more-toolbox-new');
+    CSDN_removeDom_Single('.second-recommend-box.recommend-box');
+    CSDN_removeDom_Single('.recommend-box.insert-baidu-box');
+    CSDN_removeDom_Single('.template-box');
+    CSDN_removeDom_Single('.blog-footer-bottom');
+}
+function CSDN_removeDom_Single(mycss) {
+    var myDom = document.querySelector(mycss);
+    if(myDom){myDom.parentNode.removeChild(myDom);};
+}
